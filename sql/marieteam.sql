9394afaf400125e91509c70cf33e7533cd236c93
vvuylsteker <vuylsteker@protonmail.ch>
2021-03-22 16:07:17 +0100

    Update bdd

--- sql/marieteam.sql
+++ sql/marieteam.sql
@@ -2,10 +2,10 @@
 -- version 5.0.2
 -- https://www.phpmyadmin.net/
 --
--- Hôte : 127.0.0.1:3306
--- Généré le : mer. 11 nov. 2020 à 15:56
--- Version du serveur :  5.7.31
--- Version de PHP : 7.3.21
+-- Hôte : 127.0.0.1
+-- Généré le : lun. 22 mars 2021 à 16:06
+-- Version du serveur :  10.4.14-MariaDB
+-- Version de PHP : 7.2.33
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,44 +24,38 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Structure de la table `liaison`
+-- Structure de la table `bateau`
 --
 
-DROP TABLE IF EXISTS `liaison`;
-CREATE TABLE IF NOT EXISTS `liaison` (
-  `code_liaison` int(11) NOT NULL AUTO_INCREMENT,
-  `distance` float NOT NULL,
-  `id_secteur` int(11) NOT NULL,
-  `id_port` int(11) NOT NULL,
-  `id_port_ARRIVEE` int(11) NOT NULL,
-  PRIMARY KEY (`code_liaison`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
-
---
--- Structure de la table `port`
---
-
-DROP TABLE IF EXISTS `port`;
-CREATE TABLE IF NOT EXISTS `port` (
-  `id_port` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `bateau` (
+  `id_bateau` int(11) NOT NULL,
   `nom` varchar(80) NOT NULL,
-  PRIMARY KEY (`id_port`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
+  `longueur` varchar(150) NOT NULL,
+  `largeur` varchar(150) NOT NULL,
+  `vitesse` varchar(150) NOT NULL,
+  `equipements` varchar(150) NOT NULL,
+  `image` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Structure de la table `secteur`
+-- Index pour les tables déchargées
 --
 
-DROP TABLE IF EXISTS `secteur`;
-CREATE TABLE IF NOT EXISTS `secteur` (
-  `id_secteur` int(11) NOT NULL AUTO_INCREMENT,
-  `nom` varchar(80) NOT NULL,
-  PRIMARY KEY (`id_secteur`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+--
+-- Index pour la table `bateau`
+--
+ALTER TABLE `bateau`
+  ADD PRIMARY KEY (`id_bateau`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `bateau`
+--
+ALTER TABLE `bateau`
+  MODIFY `id_bateau` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
