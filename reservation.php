45b0680b232200542fee792eb07e143b728487f6
vvuylsteker <vuylsteker@protonmail.ch>
2021-05-14 15:09:47 +0200

    Suppression des headers et ajout des includes

--- reservation.php
+++ reservation.php
@@ -28,81 +28,10 @@
                 }
             }
           }
+      
+          include('header.php');
           ?>
     <!-- header -->
-    <!doctype html>
-<html lang="fr">
-  <head>
-    <title>MarieTeam</title>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-    <link href="https://fonts.googleapis.com/css?family=K2D:400,700|Niramit:300,700" rel="stylesheet">
-
-    <link rel="stylesheet" href="css/bootstrap.css">
-    <link rel="stylesheet" href="css/animate.css">
-    <link rel="stylesheet" href="css/owl.carousel.min.css">
-    <link rel="stylesheet" href="css/bootstrap-datepicker.css">
-    <link rel="stylesheet" href="css/jquery.timepicker.css">
-
-    <link rel="stylesheet" href="fonts/ionicons/css/ionicons.min.css">
-    <link rel="stylesheet" href="fonts/fontawesome/css/font-awesome.min.css">
-
-
-    <!-- Theme Style -->
-    <link rel="stylesheet" href="css/style.css">
-    <link rel="stylesheet" href="fonts/flaticon/font/flaticon.css">
-    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@10"></script> <!-- Sweet Alert -->
-  </head>
-  <body>
-
-    <header role="banner">
-      <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <div class="container">
-          <a class="navbar-brand position-absolute" href="index.php">MarieTeam</a>
-          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample05" aria-controls="navbarsExample05" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-
-          <div class="collapse navbar-collapse position-relative" id="navbarsExample05">
-            <ul class="navbar-nav mx-auto pl-lg-5 pl-0 d-flex align-items-center">
-              <li class="nav-item">
-                <a class="nav-link" href="index.php">Accueil</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="liaisons.php">Liaisons</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="contact.php">Contact</a>
-              </li>
-              <?php
-                if(isset($_SESSION['id_utilisateur'])){
-                  ?>
-                 <li class="nav-item">
-                <a class="nav-link" href="mes-reservations.php">Mes réservations</a>
-              </li>
-              <?php if($_SESSION['grade_utilisateur'] == 1){
-                ?>
-                <li class="nav-item">
-                  <a class="nav-link" href="admin/index.php">Administration</a>
-                </li>
-                <?php
-
-              } ?>
-                <a class="nav-link" href="deconnexion.php">Déconnexion</a>
-              </li>
-              <?php }else{ ?>
-              <li class="nav-item">
-                <a class="nav-link" href="connexion_inscription.php">Connexion / Inscription</a>
-              </li>
-              <?php } ?>
-
-
-            </ul>
-          </div>
-        </div>
-      </nav>
-    </header>
     <section class="inner-page">
       <div class="slider-item py-5" style="background-image: url('img/slider-1.jpg');">
         
