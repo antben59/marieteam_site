76d39d0188f1149f39154cce792dc35ad15db7e1
vvuylsteker <vuylsteker@protonmail.ch>
2021-05-16 15:23:43 +0200

    Title edit

--- admin/header.php
+++ admin/header.php
@@ -2,7 +2,7 @@
    <!doctype html>
 <html lang="fr">
   <head>
-    <title>MarieTeam</title>
+    <title>MarieTeam Administration</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
